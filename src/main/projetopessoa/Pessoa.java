351a317586c67a06d7f427f5faf68dad6640e545
Mayconherley <mayconherley18@gmail.com>
2023-04-26 22:02:59 -0300

    Criando atributos para a classe pessoa

--- src/main/projetopessoa/Pessoa.java
+++ src/main/projetopessoa/Pessoa.java
@@ -2,8 +2,33 @@ package src.main.projetopessoa;
 
 public class Pessoa {
 
-  //TODO: CRIAR ATRIBUTOS PARA A CLASSE PESSOA: nome, idade, cpf, endereco
-  //TODO: GERAR GETTERS E SETTERS
+    //TODO: CRIAR ATRIBUTOS PARA A CLASSE PESSOA: nome, idade, cpf, endereco
+    //TODO: GERAR GETTERS E SETTERS
+    public String nome;
+    public String idade;
+    public String cpf;
 
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
